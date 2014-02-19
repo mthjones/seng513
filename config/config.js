dab1e421c4f5fe995dfdf29dbff75607f8117c93
Matthew Jones <m.t.h.jones@gmail.com>
2014-02-19 15:32:30 -0700

    Add database variables to config file

--- config/config.js
+++ config/config.js
@@ -4,7 +4,14 @@ var env = process.env.NODE_ENV || "development";
 var config = {
     development: {
         port: 9000,
-        root: path.normalize(path.join(__dirname, '..'))
+        root: path.normalize(path.join(__dirname, '..')),
+        db: {
+            adapter: 'mysql',
+            name: 'snapgram_dev',
+            port: 3306,
+            username: 'root',
+            password: null
+        }
     },
     production: {
         // Fill this in with the server information
