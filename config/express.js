589d67abf0c7019262d48f279af4de321c5e1c52
Matthew Jones <m.t.h.jones@gmail.com>
2014-02-19 10:21:56 -0700

    Set up internal server error handler

--- config/express.js
+++ config/express.js
@@ -17,6 +17,10 @@ module.exports = function(app) {
         app.use(express.json());
         app.use(express.methodOverride());
         app.use(app.router);
+        app.use(function(err, req, res, next) {
+            res.status(500);
+            res.render('500');
+        });
         app.use(function(req, res, next) {
             res.status(404);
             res.render('404');
