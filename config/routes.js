14f76fbf3d29b2304c1da95d1b77b44d0a24d794
Matthew Jones <m.t.h.jones@gmail.com>
2014-02-19 15:56:16 -0700

    Add actual user creation to user creation route

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,20 @@
+var db = require('./db');
+
 module.exports = function(app) {
     app.get('/users/new', function(req, res, next) {
         res.render('users/new');
     });
 
     app.post('/users/create', function(req, res, next) {
-        res.status(302);
-        res.location('/users/new');
-        res.send();
+        var user = db.User.build(req.body);
+        user.save()
+            .success(function() {
+                // TODO: Set a session id
+                res.redirect(302, '/feed');
+            })
+            .error(function(err) {
+                // TODO: Use connect-flash to get error message for form
+                res.redirect(302, '/users/new');
+            });
     });
 };
