ff7742404abc386f9093ed3fcf4a42edc8dc39d8
Matthew Jones <m.t.h.jones@gmail.com>
2014-02-19 10:56:57 -0700

    Set up basic redirect for user creation

--- config/routes.js
+++ config/routes.js
@@ -2,4 +2,10 @@ module.exports = function(app) {
     app.get('/users/new', function(req, res, next) {
         res.render('users/new');
     });
+
+    app.post('/users/create', function(req, res, next) {
+        res.status(302);
+        res.location('/users/new');
+        res.send();
+    });
 };
