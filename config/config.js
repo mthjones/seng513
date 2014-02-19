86a80fe8d2a513cbb6c291712fdc38371719d3cd
Matthew Jones <m.t.h.jones@gmail.com>
2014-02-19 09:31:35 -0700

    Fix up config

--- config/config.js
+++ config/config.js
@@ -1,8 +1,14 @@
 var path = require('path');
+var env = process.env.NODE_ENV || "development";
 
-module.exports = {
+var config = {
     development: {
         port: 9000,
         root: path.normalize(path.join(__dirname, '..'))
+    },
+    production: {
+        // Fill this in with the server information
     }
 };
+
+module.exports = config[env];
