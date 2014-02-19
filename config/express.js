c10fa755314393785ac3bcae4abc1771bca3ff48
Matthew Jones <m.t.h.jones@gmail.com>
2014-02-19 16:35:31 -0700

    Cleaned up express config and added passport middleware

--- config/express.js
+++ config/express.js
@@ -1,20 +1,27 @@
 var express = require('express'),
     path = require('path'),
+    passport = require('passport');
     config = require('./config');
 
 module.exports = function(app) {
     app.configure(function() {
-        app.use(express.compress());
-        app.use(express.static(path.join(config.root, 'public')));
         app.set('port', config.port.toString());
         app.set('views', path.join(config.root, 'app', 'views'));
         app.set('view engine', 'html');
         app.set('layout', 'layout');
         app.enable('view cache');
         app.engine('html', require('hogan-express'));
+
+        app.use(express.compress());
+        app.use(express.static(path.join(config.root, 'public')));
+        app.use(express.cookieParser());
         app.use(express.urlencoded());
         app.use(express.json());
         app.use(express.methodOverride());
+        app.use(express.session({secret: 'super-secret'}));
+        app.use(passport.initialize());
+        app.use(passport.session());
+
         app.use(app.router);
         app.use(function(err, req, res, next) {
             console.error(err.stack);
