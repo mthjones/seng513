388c150f496bfaa62051bbb56de04fef8789286c
Matthew Jones <m.t.h.jones@gmail.com>
2014-02-19 17:24:04 -0700

    Add null/unique validations to user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,8 @@
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define('User', {
-        name: DataTypes.STRING,
-        username: DataTypes.STRING,
-        password: DataTypes.STRING
+        name: {type: DataTypes.STRING, allowNull: false},
+        username: {type: DataTypes.STRING, unique: true, allowNull: false},
+        password: {type: DataTypes.STRING, allowNull: false}
     }, {
         instanceMethods: {
             validPassword: function(password) {
