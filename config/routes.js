ba8f489d43631212df429791af34238ff5bee560
Ragpost <robert.ag.post@gmail.com>
2014-03-02 13:22:49 -0700

    added very basic unfollow users feature

--- config/routes.js
+++ config/routes.js
@@ -9,19 +9,76 @@ module.exports = function(app) {
 
 	app.get('/users/follow', ensureLoggedIn('/sessions/new'), function(req, res, next) {
 		db.sequelize.query("SELECT * FROM Users").success(function(myTableRows) {
-            var userArray = new Array();
-            myTableRows.forEach(function(entry)
+            var currentUser = req.user.username;
+            var currentUserId = req.user.id;
+            
+            db.Follow_relation.findAll({where: {follower:currentUserId}}).success(function(relation){
+                var notFollowedUserArray = new Array();
+                var followedUserArray = new Array();
+                
+                myTableRows.forEach(function(userentry)
+                {
+                    if(userentry.username != currentUser)
+                    {
+                        
+                        var followFlag = false;
+                        if(relation != null){
+                            relation.forEach(function(followEntry)
+                            {
+                                //console.log("user id: " + userentry.id + " FollowEntry followee id: " +followEntry.followee);
+                                if(followEntry.followee == userentry.id)
+                                {
+                                    //The logged in user is already following this found user - add to unfollow list
+                                    followedUserArray.push(userentry.username);
+                                    followFlag = true;
+                                }
+                            });
+                        }
+                        if(!followFlag)
+                        {
+                            notFollowedUserArray.push(userentry.username);
+                        }
+                    }  
+                });
+                
+                res.render('users/follow', {
+                    notfollowed_userdata: notFollowedUserArray,
+                    followed_userdata: followedUserArray
+                });
+            }); 
+		});
+    });
+    
+    app.post('/users/follow/delete', ensureLoggedIn('/sessions/new'), function(req, res, next) {
+        db.User.find({where: {username: req.user.username}})
+        .success(function(user_follower) {
+            if (!user_follower)
             {
-                if(entry.username != req.user.username)
-                    userArray.push(entry.username);
+                res.redirect(302, '/feed');
+                return;
+            }
+            
+            db.User.find({where: {username: req.body.usernameInput}})
+            .success(function(user_followee) 
+            {
+                if (!user_followee)
+                {
+                    res.redirect(302, '/feed');
+                    return;
+                }
+
+               db.Follow_relation.find({where: {follower: user_follower.id, followee:user_followee.id}}).success(function(relation)
+               {
+                    relation.destroy().success(function()
+                    {
+                        //relation deleted
+                        console.log("Relation deleted");
+                    });
+               });
             });
-            
-            res.render('users/follow', {
-                userdata: userArray
-			});
-            
-			return;
-		})
+        });
+
+        res.redirect(302, '/feed');
     });
     
     app.post('/users/follow/new', ensureLoggedIn('/sessions/new'), function(req, res, next) {
