aa3fae4b60f51b6aeec1a7a61ddac7d37495f506
Matthew Jones <m.t.h.jones@gmail.com>
2014-03-04 13:12:48 -0700

    Move follow and unfollow into users controller

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -23,5 +23,82 @@ module.exports = {
                 }
                 res.redirect(302, '/users/new');
             });
+    },
+
+    view: function(req, res, next) {
+
+    },
+
+    follow: function(req, res, next) {
+        //console.log('User: ' + req.user.username + " wants to follow user: " + req.body.usernameInput);
+
+        db.User.find({where: {username: req.user.username}})
+            .success(function(user_follower) {
+                if (!user_follower)
+                {
+                    res.redirect(302, '/feed');
+                    return;
+                }
+
+                db.User.find({where: {username: req.body.usernameInput}})
+                    .success(function(user_followee)
+                    {
+                        if (!user_followee)
+                        {
+                            res.redirect(302, '/feed');
+                            return;
+                        }
+
+                        //console.log("Follower: " + user.username + " has id: " + user.id + " --- Followee: " + user2.username + " has id: " + user2.id);
+                        var newRelation = {
+                            follower: user_follower.id,
+                            followee: user_followee.id
+                        };
+                        var relationRow = db.Follow_relation.build(newRelation);
+
+                        relationRow.save().success(function()
+                        {
+                            console.log("Saved follow relation");
+                        }).error(function(error)
+                        {
+                            console.log("Error saving follow relation: " + error);
+                        });
+
+                    });
+            });
+
+        res.redirect(302, '/feed');
+    },
+
+    unfollow: function(req, res, next) {
+        db.User.find({where: {username: req.user.username}})
+            .success(function(user_follower) {
+                if (!user_follower)
+                {
+                    res.redirect(302, '/feed');
+                    return;
+                }
+
+                db.User.find({where: {username: req.body.usernameInput}})
+                    .success(function(user_followee)
+                    {
+                        if (!user_followee)
+                        {
+                            res.redirect(302, '/feed');
+                            return;
+                        }
+
+                        db.Follow_relation.find({where: {follower: user_follower.id, followee:user_followee.id}}).success(function(relation)
+                        {
+                            relation.destroy().success(function()
+                            {
+                                //relation deleted
+                                console.log("Relation deleted");
+                            });
+                        });
+                    });
+            });
+
+        res.redirect(302, '/feed');
     }
 };
--- config/routes.js
+++ config/routes.js
@@ -51,80 +51,10 @@ module.exports = function(app) {
             }); 
 		});
     });
-    
-    app.post('/users/follow/delete', ensureLoggedIn('/sessions/new'), function(req, res, next) {
-        db.User.find({where: {username: req.user.username}})
-        .success(function(user_follower) {
-            if (!user_follower)
-            {
-                res.redirect(302, '/feed');
-                return;
-            }
-            
-            db.User.find({where: {username: req.body.usernameInput}})
-            .success(function(user_followee) 
-            {
-                if (!user_followee)
-                {
-                    res.redirect(302, '/feed');
-                    return;
-                }
 
-               db.Follow_relation.find({where: {follower: user_follower.id, followee:user_followee.id}}).success(function(relation)
-               {
-                    relation.destroy().success(function()
-                    {
-                        //relation deleted
-                        console.log("Relation deleted");
-                    });
-               });
-            });
-        });
+    app.post('/users/follow/new', ensureLoggedIn('/sessions/new'), usersCtrl.follow);
+    app.post('/users/follow/delete', ensureLoggedIn('/sessions/new'), usersCtrl.unfollow);
 
-        res.redirect(302, '/feed');
-    });
-    
-    app.post('/users/follow/new', ensureLoggedIn('/sessions/new'), function(req, res, next) {
-        //console.log('User: ' + req.user.username + " wants to follow user: " + req.body.usernameInput);
-        
-        db.User.find({where: {username: req.user.username}})
-        .success(function(user_follower) {
-            if (!user_follower)
-            {
-                res.redirect(302, '/feed');
-                return;
-            }
-            
-            db.User.find({where: {username: req.body.usernameInput}})
-            .success(function(user_followee) 
-            {
-                if (!user_followee)
-                {
-                    res.redirect(302, '/feed');
-                    return;
-                }
-                
-               //console.log("Follower: " + user.username + " has id: " + user.id + " --- Followee: " + user2.username + " has id: " + user2.id);
-               var newRelation = {
-                    follower: user_follower.id,
-                    followee: user_followee.id
-               };
-               var relationRow = db.Follow_relation.build(newRelation);
-               
-               relationRow.save().success(function()
-               {
-                    console.log("Saved follow relation");
-               }).error(function(error)
-               {
-                    console.log("Error saving follow relation: " + error);
-               });
-               
-            });
-        });
-
-        res.redirect(302, '/feed');
-    });
-    
     app.get('/users/new', usersCtrl.newForm);
     app.post('/users/create', usersCtrl.create);
 
