10fb69b9074c00f88604575f6de45f19083f750d
Ragpost <robert.ag.post@gmail.com>
2014-03-04 18:31:21 -0700

    Removed bulk upload jade files, added user bulk upload functions

--- config/routes.js
+++ config/routes.js
@@ -18,35 +18,80 @@ module.exports = function(app) {
             {
                 console.log("Deleted Users table.");
             });
+            
+            db.Follow_relation.destroy().success(function()
+            {
+                console.log("Deleted Follow_relation table.");
+            });
+            
             console.log("Clear db here");
-            res.render('bulk/clear');
-        }else
-        {
-            res.redirect(302, '/feed');
+            //res.render('bulk/clear');
         }
+        
+         res.redirect(302, '/feed');
     });
     
     app.post('/bulk/users', function(req,res,next){
         if(req.query.password == config.clear_password)
         {
             console.log("bulk users post request");
-            console.log("JSON body: " + JSON.stringify(req.body));
-            res.render('bulk/clear');
-        }else
-        {
-            res.redirect(302, '/feed');
+            for(var i = 0 ; i < req.body.length;i++)
+            {
+                //For each element, create a user, for each user create follows relation:
+                var unparsedUser = req.body[i];
+                var userBody = 
+                {
+                    username: unparsedUser.name,
+                    name: unparsedUser.name,
+                    password: unparsedUser.password,
+                    id: unparsedUser.id
+                };
+                var followsLength = unparsedUser.follows.length;
+                
+                function getBody(value)
+                {
+                    return function(){req.body[value]};
+                }
+                
+                var user = db.User.build(userBody);
+                user.save().success(function() {
+                    //return res.redirect(302, '/feed');
+                }).error(function(err) {
+                    if (err.code === 'ER_DUP_ENTRY') {
+                        //req.flash('error', 'Username taken');
+                    }
+                    //res.redirect(302, '/users/new');
+                });
+                
+                for(var j = 0 ; j < followsLength;j++)
+                {
+                    var followsBody = {
+                                        followee:unparsedUser.follows[j], 
+                                        follower:unparsedUser.id 
+                                      };
+                    
+                    var f = db.Follow_relation.build(followsBody);
+                    f.save().success(function()
+                    {
+                        //console.log("Saved follow");
+                    }).error(function(error)
+                    {
+                       //error
+                    });
+                }   
+            }
         }
+        res.redirect(302, '/feed');
     });
     
     app.post('/bulk/streams', function(req,res,next){
         if(req.query.password == config.clear_password)
         {
             console.log("bulk streams post request");
-            res.render('bulk/clear');
-        }else
-        {
-            res.redirect(302, '/feed');
+            //res.render('bulk/clear');
         }
+        
+        res.redirect(302, '/feed');
     });
     
     
