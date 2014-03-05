7ed4e80fa6e9957f8ee933c9a13ec41a5e4ecc36
Ragpost <robert.ag.post@gmail.com>
2014-03-04 18:40:59 -0700

    Added bulk upload controller

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,7 @@ var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn,
     sessionsCtrl = require('../app/controllers/sessions'),
     feedCtrl = require('../app/controllers/feed'),
     photosCtrl = require('../app/controllers/photos'),
+    bulkCtrl = require('../app/controllers/bulk'),
     config = require('./config');
     
 module.exports = function(app) {
@@ -11,89 +12,9 @@ module.exports = function(app) {
         res.redirect(302, '/feed');
     });
 
-    app.get('/bulk/clear', function(req, res, next) {  
-        if(req.query.password == config.clear_password)
-        {
-            db.User.destroy().success(function()
-            {
-                console.log("Deleted Users table.");
-            });
-            
-            db.Follow_relation.destroy().success(function()
-            {
-                console.log("Deleted Follow_relation table.");
-            });
-            
-            console.log("Clear db here");
-            //res.render('bulk/clear');
-        }
-        
-         res.redirect(302, '/feed');
-    });
-    
-    app.post('/bulk/users', function(req,res,next){
-        if(req.query.password == config.clear_password)
-        {
-            console.log("bulk users post request");
-            for(var i = 0 ; i < req.body.length;i++)
-            {
-                //For each element, create a user, for each user create follows relation:
-                var unparsedUser = req.body[i];
-                var userBody = 
-                {
-                    username: unparsedUser.name,
-                    name: unparsedUser.name,
-                    password: unparsedUser.password,
-                    id: unparsedUser.id
-                };
-                var followsLength = unparsedUser.follows.length;
-                
-                function getBody(value)
-                {
-                    return function(){req.body[value]};
-                }
-                
-                var user = db.User.build(userBody);
-                user.save().success(function() {
-                    //return res.redirect(302, '/feed');
-                }).error(function(err) {
-                    if (err.code === 'ER_DUP_ENTRY') {
-                        //req.flash('error', 'Username taken');
-                    }
-                    //res.redirect(302, '/users/new');
-                });
-                
-                for(var j = 0 ; j < followsLength;j++)
-                {
-                    var followsBody = {
-                                        followee:unparsedUser.follows[j], 
-                                        follower:unparsedUser.id 
-                                      };
-                    
-                    var f = db.Follow_relation.build(followsBody);
-                    f.save().success(function()
-                    {
-                        //console.log("Saved follow");
-                    }).error(function(error)
-                    {
-                       //error
-                    });
-                }   
-            }
-        }
-        res.redirect(302, '/feed');
-    });
-    
-    app.post('/bulk/streams', function(req,res,next){
-        if(req.query.password == config.clear_password)
-        {
-            console.log("bulk streams post request");
-            //res.render('bulk/clear');
-        }
-        
-        res.redirect(302, '/feed');
-    });
-    
+    app.get('/bulk/clear', bulkCtrl.clear);
+    app.post('/bulk/users', bulkCtrl.usersUpload);
+    app.post('/bulk/streams', bulkCtrl.streamsUpload);
     
 	app.get('/users/follow', ensureLoggedIn('/sessions/new'), function(req, res, next) {
 		db.sequelize.query("SELECT * FROM Users").success(function(myTableRows) {
