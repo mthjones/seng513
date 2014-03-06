d30df31ab91e9bba96eb92fce1b0df39e8a0c67b
Matthew Jones <m.t.h.jones@gmail.com>
2014-03-06 16:11:19 -0700

    Add username to feed view

--- app/controllers/feed.js
+++ app/controllers/feed.js
@@ -1,11 +1,25 @@
-var db = require('../../config/db');
+var db = require('../../config/db'),
+    _ =  require('lodash');
 
 module.exports = {
     show: function(req, res, next) {
         var nextPage = req.query.page ? parseInt(req.query.page) + 1 : 1;
         req.user.getFeed().then(function(feed) {
             feed.getPhotoes().then(function(photos) {
-                res.render('photos/list', {photos: photos, nextPage: nextPage});
+                if (photos.length === 0) {
+                    res.render('photos/list', {photos: photos, nextPage: nextPage});
+                    return;
+                }
+                var respond = _.after(photos.length, function(photos, users) {
+                    res.render('photos/list', {photos: photos, users: users, nextPage: nextPage});
+                });
+                var users = {};
+                photos.forEach(function(photo) {
+                    photo.getUser().then(function(user) {
+                        users[photo.id] = user;
+                        respond(photos, users);
+                    });
+                });
             });
         });
     }
