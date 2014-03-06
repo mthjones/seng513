1e8762a1fb40a273b69d5faa5bef9d0cc462ee81
Matthew Jones <m.t.h.jones@gmail.com>
2014-03-06 15:53:20 -0700

    Have user's feed photos displayed

--- app/controllers/feed.js
+++ app/controllers/feed.js
@@ -2,9 +2,10 @@ var db = require('../../config/db');
 
 module.exports = {
     show: function(req, res, next) {
-        res.locals = {
-            photoRows: [[[],[],[],[],[]],[[],[],[],[],[]],[[],[],[],[],[]],[[],[],[],[],[]],[[],[],[],[],[]]]
-        };
-        res.render('photos/list');
+        req.user.getFeed().then(function(feed) {
+            feed.getPhotoes().then(function(photos) {
+                res.render('photos/list', {photos: photos});
+            });
+        });
     }
 };
