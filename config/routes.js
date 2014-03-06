c7896eebe070e18b0bfccfb071f4e70a0d00d775
Matthew Jones <m.t.h.jones@gmail.com>
2014-03-06 01:07:16 -0700

    Remove dependency on connect-ensure-login

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,15 @@
-var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn,
-    db = require('./db'),
+var db = require('./db'),
     usersCtrl = require('../app/controllers/users'),
     sessionsCtrl = require('../app/controllers/sessions'),
     feedCtrl = require('../app/controllers/feed'),
     photosCtrl = require('../app/controllers/photos'),
     bulkCtrl = require('../app/controllers/bulk'),
     config = require('./config');
+
+var ensureAuthed = function(req, res, next) {
+    if (!req.isAuthenticated()) res.redirect(302, '/sessions/new');
+    next();
+};
     
 module.exports = function(app) {
     app.get('/', function(req, res, next) {
@@ -15,8 +19,8 @@ module.exports = function(app) {
     app.get('/bulk/clear', bulkCtrl.clear);
     app.post('/bulk/users', bulkCtrl.usersUpload);
     app.post('/bulk/streams', bulkCtrl.streamsUpload);
-    
-	app.get('/users/follow', ensureLoggedIn('/sessions/new'), function(req, res, next) {
+
+    app.get('/users/follow', ensureAuthed, function(req, res, next) {
 		db.sequelize.query("SELECT * FROM Users").success(function(myTableRows) {
             var currentUser = req.user.username;
             var currentUserId = req.user.id;
@@ -62,17 +66,17 @@ module.exports = function(app) {
     app.post('/users/create', usersCtrl.create);
     app.get('/users/:id', usersCtrl.view);
 
-    app.post('/users/follow/new', ensureLoggedIn('/sessions/new'), usersCtrl.follow);
-    app.post('/users/follow/delete', ensureLoggedIn('/sessions/new'), usersCtrl.unfollow);
+    app.post('/users/follow/new', ensureAuthed, usersCtrl.follow);
+    app.post('/users/follow/delete', ensureAuthed, usersCtrl.unfollow);
 
     app.get('/sessions/new', sessionsCtrl.newForm);
     app.post('/sessions/create', sessionsCtrl.create);
-    app.get('/logout', ensureLoggedIn('/sessions/new'), sessionsCtrl.logout);
+    app.get('/logout', ensureAuthed, sessionsCtrl.logout);
 
-    app.get('/feed', ensureLoggedIn('/sessions/new'), feedCtrl.show);
+    app.get('/feed', ensureAuthed, feedCtrl.show);
 
-    app.get('/photos/new', ensureLoggedIn('/sessions/new'), photosCtrl.newForm);
-    app.post('/photos/create', ensureLoggedIn('/sessions/new'), photosCtrl.create);
-    app.get('/photos/thumbnail/:id.:ext', ensureLoggedIn('/sessions/new'), photosCtrl.thumbnail);
-    app.get('/photos/:id.:ext', ensureLoggedIn('/sessions/new'), photosCtrl.view);
+    app.get('/photos/new', ensureAuthed, photosCtrl.newForm);
+    app.post('/photos/create', ensureAuthed, photosCtrl.create);
+    app.get('/photos/thumbnail/:id.:ext', ensureAuthed, photosCtrl.thumbnail);
+    app.get('/photos/:id.:ext', ensureAuthed, photosCtrl.view);
 };
