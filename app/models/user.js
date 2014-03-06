7ecd208c60b7e5692a24a5b6b1e0b7fa5f199f2a
Matthew Jones <m.t.h.jones@gmail.com>
2014-03-06 01:00:12 -0700

    Simplify follow/unfollow concept

--- app/models/user.js
+++ app/models/user.js
@@ -17,6 +17,7 @@ module.exports = function(sequelize, DataTypes) {
         classMethods: {
             associate: function(models) {
                 User.hasMany(models.Photo);
+                User.hasMany(User, {as: 'Followee', through: 'Following'});
             }
         },
         instanceMethods: {
