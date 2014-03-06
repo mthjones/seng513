c90ef9349e9863c96181531e3b4f6b406cc17ad5
Matthew Jones <m.t.h.jones@gmail.com>
2014-03-06 15:56:45 -0700

    Add more link to feed page

--- app/controllers/feed.js
+++ app/controllers/feed.js
@@ -2,9 +2,10 @@ var db = require('../../config/db');
 
 module.exports = {
     show: function(req, res, next) {
+        var nextPage = req.query.page ? parseInt(req.query.page) + 1 : 1;
         req.user.getFeed().then(function(feed) {
             feed.getPhotoes().then(function(photos) {
-                res.render('photos/list', {photos: photos});
+                res.render('photos/list', {photos: photos, nextPage: nextPage});
             });
         });
     }
