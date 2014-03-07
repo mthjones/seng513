4acd452104e746d4bffdda405672d5a2f95ba0f5
Matthew Jones <m.t.h.jones@gmail.com>
2014-03-06 19:04:37 -0700

    Simplify feed show function

--- app/controllers/feed.js
+++ app/controllers/feed.js
@@ -4,20 +4,23 @@ var db = require('../../config/db'),
 module.exports = {
     show: function(req, res, next) {
         var page = req.query.page ? parseInt(req.query.page) : 1;
+        var render = function (photos) {
+            res.render('photos/list', {photos: photos, nextPage: page + 1});
+        };
+
         req.user.getFeed().then(function(feed) {
             feed.getPhotoes({offset: (page - 1) * 30, limit: 30}).then(function(photos) {
+                var respond = _.after(photos.length, render);
+
                 if (photos.length === 0) {
-                    res.render('photos/list', {photos: photos, nextPage: page + 1});
+                    render([]);
                     return;
                 }
-                var respond = _.after(photos.length, function(photos, users) {
-                    res.render('photos/list', {photos: photos, users: users, nextPage: page + 1});
-                });
-                var users = {};
+
                 photos.forEach(function(photo) {
                     photo.getUser().then(function(user) {
-                        users[photo.id] = user;
-                        respond(photos, users);
+                        photo.user = user;
+                        respond(photos);
                     });
                 });
             });
