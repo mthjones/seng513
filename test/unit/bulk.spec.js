31741f9965c990ddfe17ae6184258baf16fa04a6
Matthew Jones <m.t.h.jones@gmail.com>
2014-03-18 14:00:26 -0600

    Wrap #clear unit tests in their own testing block

--- test/unit/bulk.spec.js
+++ test/unit/bulk.spec.js
@@ -11,98 +11,101 @@ chai.use(sinonChai);
 chai.use(chaiAsPromised);
 
 describe('Bulk Controller', function() {
-    describe('no password specified', function() {
-        var requestMock, responseMock;
+    describe('#clear', function() {
+        describe('no password specified', function() {
+            var requestMock, responseMock;
 
-        beforeEach(function() {
-            requestMock = {query: {}};
-            responseMock = {status: sinon.spy(), send: sinon.spy()};
-        });
+            beforeEach(function() {
+                requestMock = {query: {}};
+                responseMock = {status: sinon.spy(), send: sinon.spy()};
+            });
 
-        it('sends unauthorized response', function() {
-            ctrl.clear(requestMock, responseMock);
-            expect(responseMock.status).to.have.been.calledWith(401);
-        });
+            it('sends unauthorized response', function() {
+                ctrl.clear(requestMock, responseMock);
+                expect(responseMock.status).to.have.been.calledWith(401);
+            });
 
-        it('sends the correct message', function() {
-            ctrl.clear(requestMock, responseMock);
-            expect(responseMock.send).to.have.been.calledWith("Unauthorized to clear the database");
-        });
-    });
-    describe('incorrect password', function() {
-        var requestMock, responseMock;
-
-        beforeEach(function() {
-            requestMock = {query: {password: '0000'}};
-            responseMock = {status: sinon.spy(), send: sinon.spy()};
-        });
-
-        it('sends unauthorized response', function() {
-            ctrl.clear(requestMock, responseMock);
-            expect(responseMock.status).to.have.been.calledWith(401);
-        });
-
-        it('sends the correct message', function() {
-            ctrl.clear(requestMock, responseMock);
-            expect(responseMock.send).to.have.been.calledWith("Unauthorized to clear the database");
-        });
-    });
-
-    describe('correct password', function() {
-        var requestMock, responseMock;
-
-        beforeEach(function() {
-            requestMock = {query: {password: '1234'}};
-            responseMock = {status: sinon.spy(), send: sinon.spy()};
-        });
-
-        it('calls sync with force clear', function()
-        {
-            var resolvedPromise = Promise.resolve();
-            var syncMock = sinon.stub().returns(resolvedPromise);
-            db.sequelize = {sync: syncMock};
-            ctrl.clear(requestMock, responseMock);
-            return resolvedPromise.then(function() {
-                expect(syncMock).to.have.been.calledWith({force: true});
+            it('sends the correct message', function() {
+                ctrl.clear(requestMock, responseMock);
+                expect(responseMock.send).to.have.been.calledWith("Unauthorized to clear the database");
             });
         });
-        it('sends ok response with successful clear', function() {
-            var resolvedPromise = Promise.resolve();
-            var syncMock = sinon.stub().returns(resolvedPromise);
-            db.sequelize = {sync: syncMock};
-            ctrl.clear(requestMock, responseMock);
-            return resolvedPromise.then(function() {
-                expect(responseMock.status).to.have.been.calledWith(200);
+
+        describe('incorrect password', function() {
+            var requestMock, responseMock;
+
+            beforeEach(function() {
+                requestMock = {query: {password: '0000'}};
+                responseMock = {status: sinon.spy(), send: sinon.spy()};
+            });
+
+            it('sends unauthorized response', function() {
+                ctrl.clear(requestMock, responseMock);
+                expect(responseMock.status).to.have.been.calledWith(401);
+            });
+
+            it('sends the correct message', function() {
+                ctrl.clear(requestMock, responseMock);
+                expect(responseMock.send).to.have.been.calledWith("Unauthorized to clear the database");
             });
         });
-        it('sends correct message with successful clear', function()
-        {
-            var resolvedPromise = Promise.resolve();
-            var syncMock = sinon.stub().returns(resolvedPromise);
-            db.sequelize = {sync: syncMock};
-            ctrl.clear(requestMock, responseMock);
-            return resolvedPromise.then(function() {
-                expect(responseMock.send).to.have.been.calledWith('DB cleared');
+
+        describe('correct password', function() {
+            var requestMock, responseMock;
+
+            beforeEach(function() {
+                requestMock = {query: {password: '1234'}};
+                responseMock = {status: sinon.spy(), send: sinon.spy()};
             });
-        });
-        it('sends internal server error with failed clear', function()
-        {
-            var rejectedPromise = Promise.reject();
-            var syncMock = sinon.stub().returns(rejectedPromise);
-            db.sequelize = {sync: syncMock};
-            ctrl.clear(requestMock, responseMock);
-            return expect(rejectedPromise).to.have.been.rejected.then(function() {
-                expect(responseMock.status).to.have.been.calledWith(500);
+
+            it('calls sync with force clear', function()
+            {
+                var resolvedPromise = Promise.resolve();
+                var syncMock = sinon.stub().returns(resolvedPromise);
+                db.sequelize = {sync: syncMock};
+                ctrl.clear(requestMock, responseMock);
+                return resolvedPromise.then(function() {
+                    expect(syncMock).to.have.been.calledWith({force: true});
+                });
             });
-        });
-        it('sends correct message with failed clear', function()
-        {
-            var rejectedPromise = Promise.reject();
-            var syncMock = sinon.stub().returns(rejectedPromise);
-            db.sequelize = {sync: syncMock};
-            ctrl.clear(requestMock, responseMock);
-            return expect(rejectedPromise).to.have.been.rejected.then(function() {
-                expect(responseMock.send).to.have.been.calledWith("Couldn't clear DB");
+            it('sends ok response with successful clear', function() {
+                var resolvedPromise = Promise.resolve();
+                var syncMock = sinon.stub().returns(resolvedPromise);
+                db.sequelize = {sync: syncMock};
+                ctrl.clear(requestMock, responseMock);
+                return resolvedPromise.then(function() {
+                    expect(responseMock.status).to.have.been.calledWith(200);
+                });
+            });
+            it('sends correct message with successful clear', function()
+            {
+                var resolvedPromise = Promise.resolve();
+                var syncMock = sinon.stub().returns(resolvedPromise);
+                db.sequelize = {sync: syncMock};
+                ctrl.clear(requestMock, responseMock);
+                return resolvedPromise.then(function() {
+                    expect(responseMock.send).to.have.been.calledWith('DB cleared');
+                });
+            });
+            it('sends internal server error with failed clear', function()
+            {
+                var rejectedPromise = Promise.reject();
+                var syncMock = sinon.stub().returns(rejectedPromise);
+                db.sequelize = {sync: syncMock};
+                ctrl.clear(requestMock, responseMock);
+                return expect(rejectedPromise).to.have.been.rejected.then(function() {
+                    expect(responseMock.status).to.have.been.calledWith(500);
+                });
+            });
+            it('sends correct message with failed clear', function()
+            {
+                var rejectedPromise = Promise.reject();
+                var syncMock = sinon.stub().returns(rejectedPromise);
+                db.sequelize = {sync: syncMock};
+                ctrl.clear(requestMock, responseMock);
+                return expect(rejectedPromise).to.have.been.rejected.then(function() {
+                    expect(responseMock.send).to.have.been.calledWith("Couldn't clear DB");
+                });
             });
         });
     });
