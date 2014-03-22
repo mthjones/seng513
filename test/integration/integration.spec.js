a120c3231cf149fa421e0770c6e305d4f1469345
Matthew Jones <m.t.h.jones@gmail.com>
2014-03-21 21:44:45 -0600

    Add a really simple and stupid integration test

--- test/integration/integration.spec.js
+++ test/integration/integration.spec.js
@@ -1,5 +1,5 @@
 var chai = require('chai'),
-    request = require('superagent'),
+    request = require('request'),
     expect = chai.expect;
 
 before(function(done) {
@@ -21,9 +21,25 @@ before(function(done) {
 });
 
 describe('Integration test', function() {
-    var agent = request.agent();
+    var agent = request.defaults({jar: request.jar()});
 
     describe('scenario 1', function() {
-        
+        it('works', function(done) {
+            var user = {name: 'test', username: 'test', password: 'test'};
+            // Create the user and login
+            agent.post("http://localhost:9000/users/create", {form: user}, function(err, response, body) {
+                expect(response.statusCode).to.equal(302);
+                expect(response.headers['location']).to.equal('/feed');
+                // Navigate to photo upload form
+                agent.get("http://localhost:9000/photos/new", function(err, response, body) {
+                    expect(response.statusCode).to.equal(200);
+                    // Go to a page that doesn't exist
+                    agent.get("http://localhost:9000/hello/world", function(err, response, body) {
+                        expect(response.statusCode).to.equal(404);
+                        done();
+                    });
+                });
+            });
+        });
     });
 });
