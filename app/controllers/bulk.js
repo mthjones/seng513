e1d6c0ff0998a5dc87d53537c0550c1f4a4956f8
Matthew Jones <m.t.h.jones@gmail.com>
2014-03-21 21:33:30 -0600

    Mostly fix bulk upload functions sending response before they are finished

--- app/controllers/bulk.js
+++ app/controllers/bulk.js
@@ -1,6 +1,7 @@
 var db = require('../../config/db'),
     config = require('../../config/config'),
     _ = require('lodash'),
+    Promise = require('bluebird'),
     path = require('path');
 
 var contentTypeForPath = function(filepath) {
@@ -43,14 +44,15 @@ module.exports = {
                 return;
             }
 
-            var sendResponse = _.after(users.length, responseFn);
+            var sendResponse = _.after(totalFollows, responseFn);
 
             var finishedUserCreation = _.after(users.length, function() {
                 users.forEach(function(user) {
                     user.follows.forEach(function(followeeId) {
                         db.User.find(followeeId).then(function(followee) {
                             db.User.find(user.id).then(function(follower) {
-                                followee.addFollower(follower);
+                                return followee.addFollower(follower);
+                            }).then(function() {
                                 sendResponse();
                             });
                         });
@@ -82,6 +84,7 @@ module.exports = {
             var photos = req.body;
 
             var responseFn = function () {
+                console.log('sending response');
                 res.status(200).send('Streams added');
             };
 
@@ -92,29 +95,33 @@ module.exports = {
 
             var sendResponse = _.after(photos.length, responseFn);
 
+            var idFix = photos.length && photos[0].id === 0 ? 1 : 0;
+
             photos.forEach(function(unparsedPhoto) {
                 var photoBody = {
                     createdAt: new Date(unparsedPhoto.timestamp),
                     filepath: unparsedPhoto.path,
                     name: path.basename(unparsedPhoto.path),
-                    id: unparsedPhoto.id,
+                    id: unparsedPhoto.id + idFix,
                     contentType: contentTypeForPath(unparsedPhoto.path),
                     ext: path.extname(unparsedPhoto.path).split(".").pop()
                 };
 
                 db.Photo.create(photoBody).then(function(photo) {
-                    db.User.find(unparsedPhoto.user_id).then(function(user) {
-                        user.addPhoto(photo).then(function() {
-                            user.getFeed().then(function(feed) {
-                                feed.addPhoto(photo);
-                            });
-                            user.getFollower().then(function(followers) {
-                                followers.forEach(function(follower) {
-                                    follower.getFeed().then(function(feed) {
-                                        feed.addPhoto(photo);
+                    return db.User.find(unparsedPhoto.user_id).then(function(user) {
+                        return user.addPhoto(photo).then(function() {
+                            return Promise.all([
+                                user.getFeed().then(function(feed) {
+                                    return feed.addPhoto(photo);
+                                }),
+                                user.getFollower().then(function(followers) {
+                                    return followers.forEach(function(follower) {
+                                        return follower.getFeed().then(function(feed) {
+                                            return feed.addPhoto(photo);
+                                        });
                                     });
-                                });
-                            });
+                                })
+                            ]);
                         });
                     });
                 }).then(function() {
