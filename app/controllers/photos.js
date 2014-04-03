80fa760d7ddd9605d2093ff223c93e52355e019a
Matthew Jones <m.t.h.jones@gmail.com>
2014-04-03 12:55:21 -0600

    Create thumbnail on upload

--- app/controllers/photos.js
+++ app/controllers/photos.js
@@ -2,7 +2,26 @@ var db = require('../../config/db'),
     fs = require('fs'),
     gm = require('gm'),
     _ = require('lodash'),
-    path = require('path');
+    path = require('path'),
+    Promise = require('bluebird');
+
+var createThumb = function(photo) {
+    return new Promise(function(resolve, reject) {
+        gm(fs.createReadStream(photo.filepath)).size({bufferStream: true}, function(err, size) {
+            var aspect = size.width / size.height;
+            this.resize(400, Math.round(400 / aspect)).stream(function(err, stdout) {
+                var thumbStream = fs.createWriteStream(photo.filepath + '.thumb');
+                thumbStream.on('finish', function() {
+                    resolve();
+                });
+                thumbStream.on('error', function() {
+                    reject();
+                });
+                stdout.pipe(thumbStream);
+            });
+        });
+    });
+};
 
 module.exports = {
     newForm: function(req, res, next) {
@@ -29,12 +48,7 @@ module.exports = {
             } else {
                 res.status(200);
                 res.setHeader('Content-Type', photo.contentType);
-                gm(fs.createReadStream(photo.filepath)).size({bufferStream: true}, function(err, size) {
-                    var aspect = size.width / size.height;
-                    this.resize(400, Math.round(400 / aspect)).stream(function(err, stdout) {
-                        stdout.pipe(res);
-                    });
-                });
+                fs.createReadStream(photo.filepath + '.thumb').pipe(res);
             }
         });
     },
@@ -42,18 +56,26 @@ module.exports = {
     create: function(req, res, next) {
         db.Photo.create({filepath: req.files.image.path, name: req.files.image.name, contentType: req.files.image.type, ext: path.extname(req.files.image.name).split('.').pop()}).then(function(photo) {
             return req.user.addPhoto(photo).then(function() {
-                req.user.getFeed().then(function(feed) {
-                    feed.addPhoto(photo);
-                });
-                req.user.getFollower().then(function(followers) {
-                    followers.forEach(function(follower) {
-                        follower.getFeed().then(function(feed) {
-                            feed.addPhoto(photo);
+                return Promise.all([
+                    req.user.getFeed().then(function(feed) {
+                        return feed.addPhoto(photo);
+                    }),
+                    req.user.getFollower().then(function(followers) {
+                        var followerPromises = [];
+                        followers.forEach(function(follower) {
+                            followerPromises.push(follower.getFeed().then(function(feed) {
+                                return feed.addPhoto(photo);
+                            }));
                         });
-                    });
-                });
+                        return Promise.all(followerPromises);
+                    })
+                ]);
+            }).then(function() {
+                return photo;
             });
-        }).then(function() {
+        })
+        .then(createThumb)
+        .then(function() {
             res.redirect(302, '/feed');
         }).catch(function(err) {
             console.log(err);
