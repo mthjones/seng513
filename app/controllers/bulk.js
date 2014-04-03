557b077130a36d2cd355e8491f0ef8c45827112a
Matthew Jones <m.t.h.jones@gmail.com>
2014-04-03 14:47:03 -0600

    Simplify bulk photo upload

--- app/controllers/bulk.js
+++ app/controllers/bulk.js
@@ -60,36 +60,25 @@ module.exports = {
 
     streamsUpload: function (req, res, next) {
         if (req.query.password === config.clear_password) {
-            var photos = req.body;
+            var idFix = (req.body.length && (req.body[0].id === 0)) ? 1 : 0;
+            var photos = req.body.map(function(photo) {
+                return {
+                    createdAt: new Date(photo.timestamp),
+                    filepath: photo.path,
+                    name: path.basename(photo.path),
+                    id: photo.id + idFix,
+                    contentType: contentTypeForPath(photo.path),
+                    ext: path.extname(photo.path).split(".").pop(),
+                    UserId: photo.user_id
+                }
+            });
 
-            var responseFn = function () {
-                console.log('sending response');
-                res.status(200).send('Streams added');
-            };
-
-            if (!photos.length) {
-                responseFn();
-                return;
-            }
-
-            var idFix = photos.length && photos[0].id === 0 ? 1 : 0;
-
-            var uploadPromises = [];
-
-            photos.forEach(function(unparsedPhoto) {
-                var photoBody = {
-                    createdAt: new Date(unparsedPhoto.timestamp),
-                    filepath: unparsedPhoto.path,
-                    name: path.basename(unparsedPhoto.path),
-                    id: unparsedPhoto.id + idFix,
-                    contentType: contentTypeForPath(unparsedPhoto.path),
-                    ext: path.extname(unparsedPhoto.path).split(".").pop()
-                };
-
-                uploadPromises.push(db.Photo.create(photoBody).then(function(photo) {
-                    return db.User.find(unparsedPhoto.user_id).then(function(user) {
-                        return user.addPhoto(photo).then(function() {
-                            return Promise.all([
+            db.Photo.bulkCreate(photos).then(function() {
+                var photoPromises = [];
+                photos.forEach(function(rawPhoto) {
+                    photoPromises.push(db.Photo.find(rawPhoto.id).then(function(photo) {
+                        return photo.getUser().then(function(user) {
+                            Promise.all([
                                 user.getFeed().then(function(feed) {
                                     return feed.addPhoto(photo);
                                 }),
@@ -104,12 +93,11 @@ module.exports = {
                                 })
                             ]);
                         });
-                    });
-                }));
-            });
-
-            Promise.all(uploadPromises).then(function() {
-                responseFn();
+                    }));
+                });
+                return Promise.all(photoPromises);
+            }).then(function() {
+                res.status(200).send('Streams added');
             });
         } else {
             res.status(401).send('Unauthorized to bulk add photos');
