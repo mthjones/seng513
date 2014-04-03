02666b4151483a8e79340c30c43012ec69b810e1
Matthew Jones <m.t.h.jones@gmail.com>
2014-04-03 13:09:14 -0600

    Have thumbnail created if thumbnail doesn't exist

--- app/controllers/photos.js
+++ app/controllers/photos.js
@@ -1,27 +1,9 @@
 var db = require('../../config/db'),
     fs = require('fs'),
-    gm = require('gm'),
     _ = require('lodash'),
     path = require('path'),
-    Promise = require('bluebird');
-
-var createThumb = function(photo) {
-    return new Promise(function(resolve, reject) {
-        gm(fs.createReadStream(photo.filepath)).size({bufferStream: true}, function(err, size) {
-            var aspect = size.width / size.height;
-            this.resize(400, Math.round(400 / aspect)).stream(function(err, stdout) {
-                var thumbStream = fs.createWriteStream(photo.filepath + '.thumb');
-                thumbStream.on('finish', function() {
-                    resolve();
-                });
-                thumbStream.on('error', function() {
-                    reject();
-                });
-                stdout.pipe(thumbStream);
-            });
-        });
-    });
-};
+    Promise = require('bluebird'),
+    thumbnailer = require('../../lib/thumbnailer');
 
 module.exports = {
     newForm: function(req, res, next) {
@@ -42,13 +24,25 @@ module.exports = {
     },
 
     thumbnail: function(req, res, next) {
+        var respond = function(photo) {
+            res.status(200);
+            res.setHeader('Content-Type', photo.contentType);
+            fs.createReadStream(photo.filepath + '.thumb').pipe(res);
+        };
+
         db.Photo.find(req.params.id).then(function(photo) {
             if (photo === null) {
                 res.status(404).render('404');
             } else {
-                res.status(200);
-                res.setHeader('Content-Type', photo.contentType);
-                fs.createReadStream(photo.filepath + '.thumb').pipe(res);
+                fs.exists(photo.filepath + '.thumb', function(exists) {
+                    if (exists) {
+                        respond(photo);
+                    } else {
+                        thumbnailer.createThumb(photo).then(function() {
+                            respond(photo);
+                        });
+                    }
+                });
             }
         });
     },
@@ -74,7 +68,7 @@ module.exports = {
                 return photo;
             });
         })
-        .then(createThumb)
+        .then(thumbnailer.createThumb)
         .then(function() {
             res.redirect(302, '/feed');
         }).catch(function(err) {
