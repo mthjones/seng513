1245316daa920119b31e523bbbe91cb3dcbc9141
Matthew Jones <m.t.h.jones@gmail.com>
2014-04-03 23:17:47 -0600

    Move db sync out of worker spawn
    Was causing too many connections to db

--- app.js
+++ app.js
@@ -2,29 +2,29 @@ var cluster = require('cluster');
 
 require('strong-cluster-connect-store').setup();
 
-// Adapted from http://rowanmanning.com/posts/node-cluster-and-express/
-if (cluster.isMaster) {
-    var cpuCount = require('os').cpus().length;
+var db = require('./config/db');
+db.sequelize.sync({force: false}).complete(function(err) {
+    if (err) throw err;
+    // Adapted from http://rowanmanning.com/posts/node-cluster-and-express/
+    if (cluster.isMaster) {
+        var cpuCount = require('os').cpus().length;
 
-    for (var i = 0; i < cpuCount; i++) {
-        cluster.fork();
-    }
-} else {
-    var express = require('express'),
-        config = require('./config/config'),
-        db = require('./config/db');
+        for (var i = 0; i < cpuCount; i++) {
+            cluster.fork();
+        }
+    } else {
+        var express = require('express'),
+            config = require('./config/config');
 
-    var app = express();
+        var app = express();
 
-    require('./config/passport');
-    require('./config/express')(app);
-    require('./config/routes')(app);
+        require('./config/passport');
+        require('./config/express')(app);
+        require('./config/routes')(app);
 
-    db.sequelize.sync({force: false}).complete(function(err) {
-        if (err) throw err;
         app.listen(config.port);
-    });
-}
+    }
+});
 
 cluster.on('exit', function(worker) {
     cluster.fork();
