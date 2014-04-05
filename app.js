c0b9d478e465d2a13c298e905298d7a88b164b9d
Matthew Jones <m.t.h.jones@gmail.com>
2014-04-04 23:00:09 -0600

    Move cluster connect store into master

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var cluster = require('cluster');
 
-require('strong-cluster-connect-store').setup();
-
 var db = require('./config/db');
 db.sequelize.sync({force: false}).complete(function(err) {
     if (err) throw err;
     // Adapted from http://rowanmanning.com/posts/node-cluster-and-express/
     if (cluster.isMaster) {
+        require('strong-cluster-connect-store').setup();
         var cpuCount = require('os').cpus().length;
 
         for (var i = 0; i < cpuCount; i++) {
